5647fdf2c550cb2adb0940159ac56ebc31117a1a
Shafi <shafi.cse.buet@gmail.com>
2020-06-19 12:39:36 +0600

    update to support if user wants to provide the delete_route

--- resources/views/comps/delete_form.blade.php
+++ resources/views/comps/delete_form.blade.php
@@ -1,14 +1,15 @@
 @php
   $params = [];
   $params[$resource_name] = $resource_id;
-  $route = route($resource_name . '.destroy', $params);
+  if(!isset($delete_route)) $delete_route = route($resource_name . '.destroy', $params);
   $form_id = 'form_' . $resource_name . '_delete_' . $resource_id;
 @endphp
+
 <a class="badge badge-pill text-danger"
-  href="{{ $route }}" onclick="event.preventDefault(); document.getElementById('{{ $form_id }}').submit();">
+  href="{{ $delete_route }}" onclick="event.preventDefault(); document.getElementById('{{ $form_id }}').submit();">
   Delete
 </a>
-<form id="{{ $form_id }}" action="{{ $route }}" method="POST" style="display: none;">
+<form id="{{ $form_id }}" action="{{ $delete_route }}" method="POST" style="display: none;">
   @csrf
   @method("DELETE")
 </form>
