65c051692a2ac99e64915ed48f0788002b482d7d
Shafi <shafi.cse.buet@gmail.com>
2020-06-19 15:22:37 +0600

    add cancel button with form save button

--- resources/views/comps/btn_submit.blade.php
+++ resources/views/comps/btn_submit.blade.php
@@ -1,7 +1,10 @@
 <div class="form-group row mb-0">
   <div class="col-md-8 offset-md-4">
-    <button type="submit" class="btn btn-primary">
+    <button type="submit" class="btn btn-outline-primary">
       {{ __($btn_submit_txt ?? 'Save') }}
     </button>
+    <a href="{{ url()->previous() }}" class="btn btn-outline-secondary">
+      {{ __($btn_cancel_txt ?? 'Cancel') }}
+    </a>
   </div>
 </div>
