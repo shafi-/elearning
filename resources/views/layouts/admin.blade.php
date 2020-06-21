f689f2073eda7ccaa58d7a07799e12c5f2e2658d
Shafi <shafi.cse.buet@gmail.com>
2020-06-21 09:15:51 +0600

    add js, css section to admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
   </head>
   <body>
     <div id="app">
@@ -33,5 +33,7 @@
         </div>
       </main>
     </div>
+
+    @yield('js')
   </body>
 </html>
