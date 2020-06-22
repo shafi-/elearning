4b296a909d9e3af16a8cc082db5bb15ec36037ed
Shafi <shafi.cse.buet@gmail.com>
2020-06-22 17:43:07 +0600

    exam submit handler refactored

--- app/Exam.php
+++ app/Exam.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
@@ -13,6 +14,45 @@ class Exam extends Model
     public function answers()
     {
         return $this->hasMany(Answer::class);
+    }
+
+    /**
+     * @param array $submissions
+     * @param int $submissions.id
+     * @param int[] $submission.answers
+     */
+    public function saveAnswers($answers)
+    {
+        // In the case of resubmission, we care only the latest submissions from the user
+        // So first we remove all previous answers
+        // And then save the new answers
+        // In case of failure, using transaction so we do not lose previous submissions
+        DB::transaction(function() use($answers){
+            $this->answers()->delete();
+            $this->answers()->saveMany($answers);
+        });
+    }
+
+    /**
+     * 
+     */
+    public function calculate_score($answers = null)
+    {
+        if (is_null($answers)) {
+            $answers = $this->answers;
+        }
+
+        $results = collect($answers)->groupBy('mcq_id')->map(function ($answers, $mcq_id) {
+            return [
+                'mcq_id' => $mcq_id,
+                'verdict' => $answers->every(function($ans) { return $ans->verdict; }),
+                'answers' => $answers,
+            ];
+        });
+
+        $score = $results->filter(function($result) { return $result['verdict']; })->count();
+
+        return [ 'answers' => $results, 'score' => $score ];
     }
 
     public function user()
--- app/Http/Controllers/Api/ExamController.php
+++ app/Http/Controllers/Api/ExamController.php
@@ -27,49 +27,35 @@ class ExamController extends Controller
             'mcqs.*.answers.*' => 'numeric',
         ]);
 
-        $mcqs = Mcq::with('options')->where('lesson_id', $exam->lesson_id)->get()->keyBy('id');
+        $mcqs = $exam->lesson->mcqs()->with('options')->get()->keyBy('id');
 
-        $submissions = $request->get('mcqs');
+        $answers = $this->prepare_answers($request->get('mcqs'), $mcqs);
 
+        $exam->saveAnswers($answers);
+
+        $results = $exam->calculate_score($answers);
+        $results['total'] = $mcqs->count();
+
+        return $results;
+    }
+
+    private function prepare_answers(array $submissions, $mcqs)
+    {
         $answers = [];
 
-        foreach ($submissions as $key => $submission) {
+        foreach ($submissions as $submission) {
             $mcq = $mcqs[$submission['id']];
             $corrects = $mcq->options->filter(function($opt) { return $opt->is_answer; })->keyBy('id');
 
             foreach($submission['answers'] as $answer) {
                 $answers[] = new Answer([
                     'mcq_id' => $submission['id'],
-                    'exam_id' => $exam->id,
                     'option_id' => $answer,
                     'verdict' => isset($corrects[$answer])
                 ]);
             }
         }
 
-        // In the case of resubmission, we care only the latest submissions from the user
-        // So first we remove all previous answers
-        // And then save the new answers
-        // In case of failure, using transaction so we do not lose previous submissions
-        DB::transaction(function() use($exam, $answers){
-            $exam->answers()->delete();
-            $exam->answers()->saveMany($answers);
-        });
-
-        $results = collect($answers)->groupBy('mcq_id')->map(function ($answers, $mcq_id) {
-            return [
-                'mcq_id' => $mcq_id,
-                'verdict' => $answers->every(function($ans) { return $ans->verdict; }),
-                'answers' => $answers,
-            ];
-        });
-
-        $score = $results->filter(function($result) { return $result['verdict']; })->count();
-
-        return [
-            'answers' => $results,
-            'score' => $score,
-            'total' => $mcqs->count()
-        ];
+        return $answers;
     }
 }
