d0346f32eef9d87e9f9aa41d70b02fb08f745f72
Shafi <shafi.cse.buet@gmail.com>
2020-06-23 13:12:58 +0600

    add check-admin middleware

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() && Auth::user()->is_admin())
+            return $next($request);
+        abort(403, 'Unauthorized action.');
+    }
+}
