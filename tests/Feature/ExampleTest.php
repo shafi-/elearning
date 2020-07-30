2394c43bed522f8670d2966a4a9eb981c926e11d
shafi- <shafi.cse.buet@gmail.com>
2020-07-31 00:11:28 +0600

    Test set up

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,8 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -14,8 +16,19 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api');
+
+        $response->assertNotFound();
+    }
+
+    public function testAuthUser()
+    {
+        $user = factory(\App\User::class)->create();
+
+        $response = $this->actingAs($user, 'api')->get('/api/user');
 
         $response->assertStatus(200);
+
+        $response->assertJson($user->toArray());
     }
 }
